0e3476d1a6f604971611256b3420d21a43cc8084
Andraz <andraz.pirnovar@gmail.com>
2018-07-08 11:47:21 +0200

    Migracija funkcij
    Zaradi preglednosti funkcije migrirane v svojo datoteko

--- server/server.R
+++ server/server.R
@@ -1,100 +1,9 @@
-library(dbplyr)
-#source("auth.R")
-
-source("../auth.R")
-sign.up.user <- function(name, surname, address, city, country, emso, mail, username, pass){
-  # Return values:
-  # 1 ... success
-  # 0 ... error
-  # -10 ... username exists
-  success <- 0      # Boolean, if the insertion into the db was successful
-  
-  useraccount <- data.frame(name, surname, address, city, country, emso, mail, username, password=pass)
-  tryCatch({
-    drv <- dbDriver("PostgreSQL")
-    conn <- dbConnect(drv, dbname = db, host = host, user = user, password = password)
-    userTable <- tbl(conn, "useraccount")
-    # Checks if username is already in the database
-    if(0 != dim((userTable %>% filter(username == username) %>% collect()))[1]){
-      success <- -10
-    }
-    dbWriteTable(conn,name="useraccount", useraccount, append=TRUE, row.names = FALSE)
-    success <- 1
-  
-  }, finally = {
-    dbDisconnect(conn)
-    return(success)
-  })
-}
-
-sign.in.user <- function(username, pass){
-  # Return a list. In the first place is an indicator of success:
-  # 1 ... success
-  # 0 ... error
-  # -10 ... wrong username
-  # The second place represents the userid if the login info is correct,
-  # otherwise it's NULL
-  success <- 0
-  uporabnikID <- NULL
-  tryCatch({
-    drv <- dbDriver("PostgreSQL")
-    conn <- dbConnect(drv, dbname = db, host = host, user = user, password = password)
-    userTable <- tbl(conn, "useraccount")
-    # obstoj = 0, ce username in geslo ne obstajata,  1 ce obstaja
-    obstoj <- dim((userTable %>% filter(username == username, password == pass) %>% collect()))[1]
-    if(obstoj == 0){
-      success <- -10
-    }else{
-        uporabnikID <- (userTable %>% filter(username == username, password == pass) %>%
-                        select(userid) %>% collect())[[1]]
-        success <- 1
-    }
-  },warning = function(w){
-    print(w)
-  },error = function(e){
-    print(e)
-  }, finally = {
-    dbDisconnect(conn)
-    return(list(success, uporabnikID))
-  })
-}
-
-pridobi.imena.mack <- function(){
-  # Pridobi imena mack iz baze in jih vrne
-  tryCatch({
-    drv <- dbDriver("PostgreSQL")
-    conn <- dbConnect(drv, dbname = db, host = host, user = user, password = password)
-    sqlInput<- paste("SELECT DISTINCT breed FROM cat")
-    
-    macke <- dbGetQuery(conn, sqlInput)
-  },finally = {
-    dbDisconnect(conn)
-    return(unname(unlist(macke)))
-  }
-  )
-}
-
-execute.buy.order <- function(cat, price, quantity, userID){
-  tryCatch({
-      drv <- dbDriver("PostgreSQL")
-      conn <- dbConnect(drv, dbname = db, host = host, user = user, password = password)
-      userTable <- tbl(conn, "useraccount")
-      #TODO
-    },warning = function(w){
-      print(w)
-    },error = function(e){
-      print(e)
-    }, finally = {
-      dbDisconnect(conn)
-      #TODO
-  })
-}
-  
 #drv <- dbDriver("PostgreSQL")
 #conn <- dbConnect(drv, dbname = db, host = host, user = user, password = password)
 #dbDisconnect(conn)
 shinyServer(function(input, output){
   source("../auth.R")
+  source("serverFunctions.R")
   library(dplyr)
   library(dbplyr)
   library(DBI)
@@ -192,5 +101,11 @@ shinyServer(function(input, output){
 }
 )
 
-#sign.up.user("name", "surname", "addres", "city", "country", "emso", "mail", "username6","password")
+# sign.up.user("name", "surname", "addres", "city", "country", "emso", "mail", "username1","password")
+# sign.up.user("name", "surname", "addres", "city", "country", "emso", "mail", "username2","password")
+# sign.up.user("name", "surname", "addres", "city", "country", "emso", "mail", "username3","password")
+# sign.up.user("name", "surname", "addres", "city", "country", "emso", "mail", "username4","password")
+# sign.up.user("name", "surname", "addres", "city", "country", "emso", "mail", "username5","password")
+# sign.up.user("name", "surname", "addres", "city", "country", "emso", "mail", "username6","password")
+# sign.up.user("name", "surname", "addres", "city", "country", "emso", "mail", "username7","password")
 
