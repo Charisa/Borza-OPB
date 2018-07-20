a7ae33a94c9f10fad616b08ed4bc60783b1cde4e
Andraz <andraz.pirnovar@gmail.com>
2018-07-21 01:25:43 +0200

    pridobi.cene.macke
    Dodana funkcija, ki pridobi tabelo cen izbrane macke.

--- server/serverFunctionsTemp.R
+++ server/serverFunctionsTemp.R
@@ -1,7 +1,19 @@
-pridobi.cene.macke <- function(cat, ID){
-  # TODO realna funkcija za pridobitev cen
-  df <- data.frame(test1 = 1, test2 = cat)
-  df
+pridobi.cene.macke <- function(cat){
+  tryCatch({
+    drv <- dbDriver("PostgreSQL")
+    conn <- dbConnect(drv, dbname = db, host = host, user = user, password = password)
+    sqlInput<- build_sql("SELECT breed, price, SUM(current) AS number FROM orderbook 
+                          LEFT JOIN cat ON orderbook.catid = cat.catid
+                          WHERE current>0 AND cat.breed = ",cat,"
+                          GROUP BY breed, price
+                          ORDER BY price ASC;")
+    
+    macke <- dbGetQuery(conn, sqlInput)
+  },finally = {
+    dbDisconnect(conn)
+    return(macke)
+  }
+  )
 }
 
 pridobi.ime.uporabnika <- function(userID){
@@ -9,7 +21,7 @@ pridobi.ime.uporabnika <- function(userID){
   tryCatch({
     drv <- dbDriver("PostgreSQL")
     conn <- dbConnect(drv, dbname = db, host = host, user = user, password = password)
-    sqlInput<- paste("SELECT username FROM useraccount WHERE userid=",userID)
+    sqlInput<- build_sql("SELECT username FROM useraccount WHERE userid=",userID)
     
     userid <- dbGetQuery(conn, sqlInput)
   },finally = {
