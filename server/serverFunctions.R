0e2346294e5845ec36fb1c42245a29243ab0dcd7
Charisa <korat.sara7@gmail.com>
2018-07-22 07:58:03 +0200

    Izbrisan duplikat

--- server/serverFunctions.R
+++ server/serverFunctions.R
@@ -151,30 +151,6 @@ user.change.balance <- function(userID, quantity, type){
   })
 }
 
-user.change.balance <- function(userID, quantity, type){
-  tryCatch({
-    drv <- dbDriver("PostgreSQL")
-    conn <- dbConnect(drv, dbname = db, host = host, user = user, password = password)
-    status <- FALSE
-    if ((type == "withdraw" | type ==  "bought") & check.wallet.balance(userID) < quantity){
-      status <- FALSE
-    } else {
-      sqlInput <- build_sql("INSERT INTO wallet VALUES (", userID, ",", quantity, ",", type, ");")
-      dbGetQuery(conn, sqlInput)
-      status <- TRUE
-    }
-  },warning = function(w){
-    print(w)
-    status <- "napaka"
-  },error = function(e){
-    print(e)
-    status <- "napaka"
-  }, finally = {
-    dbDisconnect(conn)
-    return(status)
-  })
-}
-
 check.total.price <- function(cat, quantity){
   tryCatch({
     drv <- dbDriver("PostgreSQL")
@@ -294,6 +270,7 @@ pridobi.zgodovino.transakcij <- function(userID){
   }
     )
 }
+
 
 
 
